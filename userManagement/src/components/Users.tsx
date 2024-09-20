e859a9b18337e758435c9144d48e5e745e79e7cb
subarts <subbota1991@gmail.com>
2024-09-20 10:44:43 +0400

    fix: li wrapped in ul

--- userManagement/src/components/Users.tsx
+++ userManagement/src/components/Users.tsx
@@ -15,11 +15,15 @@ const Users: React.FC<props> = ({ users, editUser, deleteUser }) => {
   const usersList = users.map((el: User) => {
     return (
       <div key={el.id} className="user">
-        <li className="user__id">{el.id}</li>
-        <li className="user__name">name:{el.name}</li>
-        <li className="user__email">email:{el.email}</li>
-        <button onClick={deleteUser}>удалить</button>
-        <button onClick={editUser}>редактировать </button>
+        <ul>
+          <li className="user__id">{el.id}</li>
+          <li className="user__name">name:{el.name}</li>
+          <li className="user__email">email:{el.email}</li>
+        </ul>
+        <div className="user__buttons">
+          <button onClick={deleteUser}>удалить</button>
+          <button onClick={editUser}>редактировать </button>
+        </div>
       </div>
     )
   })
